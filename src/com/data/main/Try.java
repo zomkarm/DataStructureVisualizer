1ff0e40ddfc29c41955a69e4c33bb18bac712623
zomkarm9992 <zendeomkarm@rediffmail.com>
2020-08-19 22:36:09 +0530

    second commit

--- src/com/data/main/Try.java
+++ src/com/data/main/Try.java
@@ -13,24 +13,8 @@ import java.util.ArrayList;
  * @author omkar
  */
 public class Try {
-    static ArrayList<Integer> bIndex=new ArrayList<Integer>();
     
-    public void index(int virtualArray[],int element){
-        int left=0,right=virtualArray.length;
-        while(left<=right){
-           int mid=(left+right)/2;
-           bIndex.add(mid);
-           if(virtualArray[mid]==element){
-               return;
-           }
-           if(element>virtualArray[mid]) left=mid+1;
-           else right=mid-1;
-        }
-    }
     public static void main(String[] args) {
-        Try t=new Try();
-        int arr[]={1,3,4,5,6,7,8,9};
-        t.index(arr, 3);
-        for(Integer a:bIndex) out.println(a);
+        new Tet().setVisible(true);
     }
 }
--- src/com/data/view/DashBoard.java
+++ src/com/data/view/DashBoard.java
@@ -32,6 +32,7 @@ public class DashBoard extends javax.swing.JFrame {
         SortButton = new javax.swing.JButton();
         DashHeading = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
+        StackButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(158, 158, 136));
@@ -71,6 +72,14 @@ public class DashBoard extends javax.swing.JFrame {
             .addGap(0, 9, Short.MAX_VALUE)
         );
 
+        StackButton.setFont(new java.awt.Font("Manjari", 0, 18)); // NOI18N
+        StackButton.setText("Stack");
+        StackButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                StackButtonActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -79,13 +88,14 @@ public class DashBoard extends javax.swing.JFrame {
                 .addGap(0, 161, Short.MAX_VALUE)
                 .addComponent(DashHeading, javax.swing.GroupLayout.PREFERRED_SIZE, 640, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(136, 136, 136))
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(367, 367, 367)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(StackButton, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(SearchButton, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(SortButton, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -94,11 +104,13 @@ public class DashBoard extends javax.swing.JFrame {
                 .addComponent(DashHeading, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(69, 69, 69)
+                .addGap(50, 50, 50)
+                .addComponent(StackButton, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(48, 48, 48)
                 .addComponent(SearchButton, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addGap(55, 55, 55)
                 .addComponent(SortButton, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(200, Short.MAX_VALUE))
+                .addContainerGap(95, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -131,10 +143,16 @@ public class DashBoard extends javax.swing.JFrame {
         this.setVisible(false);
     }//GEN-LAST:event_SortButtonActionPerformed
 
+    private void StackButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_StackButtonActionPerformed
+        new StackFrame().setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_StackButtonActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel DashHeading;
     private javax.swing.JButton SearchButton;
     private javax.swing.JButton SortButton;
+    private javax.swing.JButton StackButton;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
